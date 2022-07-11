e5c33188e5ed5fe68a7aa760a88979facc671bf1
jsk890 <resqme89@naver.com>
2022-07-11 20:18:10 +0900

    아!

---
+++ Bronze/4999.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+# 4999 아!
+# 한줄 입력 받기
+# lst = [int(input()) for _ in range(10)]
+
+a = input()
+b = input()
+if len(a) < len(b):
+    print("no")
+else:
+    print("go")
+
+
+
